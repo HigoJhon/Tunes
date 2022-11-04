fc210fbfcd5b7bf24cf9a82413344ae4b665c44a
HigoJhon <higojhon98@gmail.com>
2022-11-04 18:42:31 -0300

    Criando um componente de cabeçalho

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
   state = {
     userName: '',
+    loading: true,
   };
 
   async componentDidMount() {
     const name = await getUser();
     this.setState({
       userName: name,
+      loading: false,
     });
   }
 
   render() {
-    const { userName } = this.state;
+    const { userName, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <p data-testid="header-user-name">
-          { userName }
-        </p>
+        {
+          loading ? <Loading /> : (
+            <p data-testid="header-user-name">
+              {userName.name }
+            </p>
+          )
+        }
+
       </header>
     );
   }
